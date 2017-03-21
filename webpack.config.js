c057dacfa72503cb52c2d9c89337fa62c9cf10e3
Neil Ruiz Dorador <nruizd.developer@gmail.com>
2017-03-21 14:19:44 -0500

    add build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require("path");
 const dirApp = path.join(__dirname, 'src');
 
-module.exports = {
+ const config = {
   entry: {
     main: "app.js",
 
@@ -24,25 +24,6 @@ module.exports = {
   devtool: 'eval',
   module: {
     rules: [
-      /*{
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: ['css-loader','sass-loader'],
-          publicPath: '/css/'
-        })
-      },*/
-        {
-          test: /\.scss$/,
-          use: [
-          {
-            loader: "style-loader" // creates style nodes from JS strings
-          }, {
-            loader: "css-loader" // translates CSS into CommonJS
-          }, {
-            loader: "sass-loader" // compiles Sass to CSS
-          }]
-        },
         {
           test: /\.js$/,
           exclude: /(node_modules|bower_components)/,
@@ -69,34 +50,66 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    /*new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true,
-      },
-      output: {
-        comments: false,
-      },
-    }),*/
     new HtmlWebpackPlugin({
       title: 'Inicio | Luna',
       inject: true,
       hash: true,
       template: './src/pug/index.pug', // Load a custom template (ejs by default see the FAQ for details)
     }),
-    new ExtractTextPlugin({
-      filename: 'css/[name].css',
-      disable: false,
-      allChunks: true
-    }),
     new webpack.NoEmitOnErrorsPlugin()
   ]
 }
+
+if (process.env.NODE_ENV == 'production') {
+
+   let cfg = [
+     new webpack.optimize.UglifyJsPlugin({
+       compress: {
+         warnings: false,
+         screw_ie8: true,
+         conditionals: true,
+         unused: true,
+         comparisons: true,
+         sequences: true,
+         dead_code: true,
+         evaluate: true,
+         if_return: true,
+         join_vars: true,
+       },
+       output: {
+         comments: false,
+       },
+     }),
+     new ExtractTextPlugin({
+       filename: 'css/[name].css',
+       disable: false,
+       allChunks: true
+     })
+   ]
+
+  config.plugins = config.plugins.concat(cfg)
+
+  config.module.rules.push(
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader','sass-loader'],
+          publicPath: '/css/'
+        })
+      }
+  );
+}else{
+  config.module.rules.push(
+    {
+      test: /\.scss$/,
+      use: [
+        { loader: "style-loader" },
+        { loader: "css-loader" },
+        { loader: "sass-loader"}
+      ]
+    }
+  )
+}
+
+module.exports = config
